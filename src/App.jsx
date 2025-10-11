89ba38f89375a7f0b81c06d19e680f6922266a7b
unknown <mhmdzratpyshh632@gmail.com>
2025-10-11 22:24:46 +0400

    Create initial templete and style it

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,40 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='bg-white py-12 px-5 w-110'>
+       <div class="container" className='flex flex-col gap-3 items-center justify-center'>
+        <h1 id="main-title" className='text-blue-900 font-bold text-3xl'>TO DO LIST</h1>
+        <input id="input-head" placeholder="search..." className='bg-blue-100 px-2 py-1 rounded-4xl w-full mb-4'/>
+        <ul className='flex flex-col gap-3.5 mb-3'>
+            <h3>Your list</h3>
+            <li class="li">
+                <span>clean up your room </span>
+                <button class="btn">delete</button>
+            </li>
+            <li class="li">
+                <span>find a motivational book foe reading</span>
+                <button class="btn">delete</button>
+            </li>
+            <li class="li">
+                <span>go to the gym</span> 
+                <button class="btn">delete</button>
+            </li>
+            <li class="li">
+                <span>focus on your goals</span> 
+                <button class="btn">delete</button>
+            </li>
+        </ul>
+        <form>
+            <input id="input-button" className='bg-blue-100 px-2 py-1 rounded-4xl mr-2'/>
+            <button className='bg-blue-800 px-2.5 py-1.5 text-white rounded-xl'>add</button>
+        </form>
       </div>
-      <h1 className='text-red-500'>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)} className='text-red-500'>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
